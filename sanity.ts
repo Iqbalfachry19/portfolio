81944c70366eb79395d6366f11295fe3dc205375
iqbal <ifachry@rocketmail.com>
2023-11-23 17:33:24 +0700

    fix

--- sanity.ts
+++ sanity.ts
@@ -5,7 +5,7 @@ export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   apiVersion: '2023-05-03',
-  useCdn: true,
+  useCdn: false,
 };
 
 export const sanityClient = createClient(config);
