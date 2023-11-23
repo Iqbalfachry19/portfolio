2b02fae30d07d2955f909542dfd9106993d77fb2
iqbal <ifachry@rocketmail.com>
2023-11-23 16:44:04 +0700

    fix

--- utils/FetchExperiences.ts
+++ utils/FetchExperiences.ts
@@ -1,7 +1,7 @@
 import { Experience } from '../typings';
 export const fetchExperiences = async () => {
   const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`, { cache: 'no-store' }
   );
   const data = await res.json();
   const experiences: Experience[] = data.experiences;
--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -1,6 +1,6 @@
 import { Skill } from '../typings';
 export const fetchSkills = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`, { cache: 'no-store' });
   const data = await res.json();
   const skills: Skill[] = data.skills;
   return skills;
