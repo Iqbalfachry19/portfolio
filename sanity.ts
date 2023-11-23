5de020b6b2fa184f164cbd7f41a0ad39140c4c37
iqbalfachry19 <ifachry@rocketmail.com>
2023-11-23 13:12:48 +0700

    fix

--- sanity.ts
+++ sanity.ts
@@ -4,7 +4,7 @@ import createImageUrlBuilder from '@sanity/image-url';
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  apiVerison: '2021-03-25',
+  apiVerison: '2021-08-31',
   useCdn: process.env.NODE_ENV === 'production',
 };
 
