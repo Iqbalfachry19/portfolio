25fb8498b020c6001434df0633331ce42c826bd9
iqbal <ifachry@rocketmail.com>
2023-11-23 17:10:06 +0700

    fix

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -3,6 +3,7 @@ export const fetchExperiences = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperiences`, { cache: 'no-store' }
   );
+  if (res.headers.get("Content-Type") !== "application/json") return
   const data = await res.json();
   const experiences: Experience[] = data.experiences;
   return experiences;
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -3,6 +3,7 @@ export const fetchProjects = async () => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`, { cache: 'no-store' }
   );
+  if (res.headers.get("Content-Type") !== "application/json") return
   const data = await res.json();
   const projects: Project[] = data.projects;
   return projects;
