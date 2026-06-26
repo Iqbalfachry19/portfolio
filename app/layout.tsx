45be3e3d2d43f63d136db8fa007b3836b71b016e
iqbal <ifachry@rocketmail.com>
2026-06-26 21:14:04 +0700

    fix logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta name="description" content="Iqbal's Portfolio" />
+        <meta
+          name="keywords"
+          content="Iqbal, Portfolio, Web Development, React, Next.js, Sanity, Tailwind CSS, TypeScript"
+        />
+        <meta name="author" content="Iqbal Fachry" />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index, follow" />
+        <meta name="bingbot" content="index, follow" />
+        <meta name="yandexbot" content="index, follow" />
+      </head>
       <body>{children}</body>
     </html>
   );
