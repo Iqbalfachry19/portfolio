cf4ab84d966bf2b93db85870d5ac3c4c9a79159d
iqbal <ifachry@rocketmail.com>
2026-06-26 20:48:44 +0700

    fix portfolio

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -12,19 +12,22 @@ const Skills = ({ skills }: Props) => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className=" flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+      className="flex flex-col items-center justify-center min-h-screen max-w-[2000px] mx-auto xl:px-10"
     >
-      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl">
-        Skills
-      </h3>
-      <h3 className="absolute top-24 uppercase tracking-[3px] text-gray-500 text-sm">
-        Hover over a skill for current profieciency
-      </h3>
-      <div className="grid mt-28 grid-cols-4 gap-5 pr-16 pt-10">
+      <div className="text-center mb-12">
+        <h3 className="uppercase tracking-[20px] text-gray-500 text-2xl">
+          Skills
+        </h3>
+        <p className="mt-4 uppercase tracking-[3px] text-gray-500 text-sm">
+          Hover over a skill for current proficiency
+        </p>
+      </div>
+
+      <div className="grid grid-cols-4 gap-5">
         {skills?.slice(0, skills.length / 2).map((skill) => (
           <Skill key={skill._id} skill={skill} />
         ))}
-        {skills?.slice(skills.length / 2, skills.length).map((skill) => (
+        {skills?.slice(skills.length / 2).map((skill) => (
           <Skill key={skill._id} skill={skill} directionLeft />
         ))}
       </div>
